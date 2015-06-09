07c26c6ecfe8af9d061f804ccd5f234982e2d53b
Te MA <te.matt.ma@gmail.com>
2015-06-09 22:14:09 +1000

    Initial commit

---
+++ src/com/opemind/mathgame/MainActivity.java
@@ -0,0 +1,33 @@
+package com.opemind.mathgame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button buttonPlay = (Button)findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+}
